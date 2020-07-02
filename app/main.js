2fa91f8e247d6e78aca20a813b5f4090d93c6c3d
Balpreet Singh <knowbalpreet@Balpreets-MacBook-Pro.local>
2020-07-03 01:34:58 +0530

    adding support for opening and saving markdown files and updated dependencies

--- app/main.js
+++ app/main.js
@@ -1,1 +1,80 @@
-console.log('This is an Electron application!');
+const fs = require("fs");
+const { app, BrowserWindow, dialog } = require("electron");
+
+let mainWindow = null;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+});
+
+exports.getFileFromUser = async () => {
+  const files = await dialog.showOpenDialog(mainWindow, {
+    properties: ["openFile"],
+    buttonLabel: "Unveil",
+    title: "Open Fire Sale Document",
+    filters: [
+      {
+        name: "Markdown Files",
+        extensions: ["md", "mdown", "markdown", "marcdown"],
+      },
+      { name: "Text Files", extensions: ["txt", "text"] },
+    ],
+  });
+  if (files.canceled) return;
+
+  const file = files.filePaths[0];
+
+  openFile(file);
+};
+
+exports.saveFile = async (file, content) => {
+  if (!file) {
+    let currentFile = await dialog.showSaveDialog(mainWindow, {
+      title: "Save Markdown",
+      defaultPath: app.getPath("desktop"),
+      filters: [
+        {
+          name: "Markdown Files",
+          extensions: ["md", "mdown", "markdown", "marcdown"],
+        },
+      ],
+    });
+    if (currentFile.canceled) return;
+    file = currentFile.filePath;
+  }
+  fs.writeFileSync(file, content);
+  openFile(file);
+};
+
+exports.saveHTML = async (content) => {
+  let currentFile = await dialog.showSaveDialog(mainWindow, {
+    title: "Save HTML",
+    defaultPath: app.getPath("desktop"),
+    filters: [
+      {
+        name: "HTML Files",
+        extensions: ["html", "htm"],
+      },
+    ],
+  });
+  if (currentFile.canceled) return;
+  fs.writeFileSync(currentFile.filePath, content);
+};
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  app.addRecentDocument(file);
+
+  mainWindow.webContents.send("file-opened", file, content);
+};
--- app/renderer.js
+++ app/renderer.js
@@ -1,20 +1,71 @@
-const marked = require('marked');
+const path = require("path");
+const marked = require("marked");
+const { remote, ipcRenderer } = require("electron");
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
-const showFileButton = document.querySelector('#show-file');
-const openInDefaultButton = document.querySelector('#open-in-default');
+let filePath = null;
+let originalContent = "";
 
-const renderMarkdownToHtml = markdown => {
+const mainProcess = remote.require("./main");
+const currentWindow = remote.getCurrentWindow();
+
+const markdownView = document.querySelector("#markdown");
+const htmlView = document.querySelector("#html");
+const newFileButton = document.querySelector("#new-file");
+const openFileButton = document.querySelector("#open-file");
+const saveMarkdownButton = document.querySelector("#save-markdown");
+const revertButton = document.querySelector("#revert");
+const saveHtmlButton = document.querySelector("#save-html");
+const showFileButton = document.querySelector("#show-file");
+const openInDefaultButton = document.querySelector("#open-in-default");
+
+const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
-markdownView.addEventListener('keyup', event => {
+const updateUserInterface = (isEdited) => {
+  let title = "Fire Sale";
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+
+  if (isEdited) {
+    title += " *";
+  }
+
+  if (filePath) currentWindow.setRepresentedFilename(filePath);
+  currentWindow.setDocumentEdited(isEdited);
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  currentWindow.setTitle(title);
+};
+
+markdownView.addEventListener("keyup", (event) => {
   const currentContent = event.target.value;
+
   renderMarkdownToHtml(currentContent);
+  updateUserInterface(currentContent !== originalContent);
+});
+
+openFileButton.addEventListener("click", () => {
+  mainProcess.getFileFromUser();
+});
+
+saveMarkdownButton.addEventListener("click", () => {
+  mainProcess.saveFile(filePath, markdownView.value);
+});
+
+saveHtmlButton.addEventListener("click", () => {
+  mainProcess.saveHTML(htmlView.innerHTML);
+});
+
+ipcRenderer.on("file-opened", (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+
+  updateUserInterface(false);
 });
